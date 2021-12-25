d75ae73cc8ec5766b47bbf323ec990e617d92c45
Krishalika <krishalikadilanirathnayake@gmail.com>
2021-12-26 00:49:54 +0530

    added crossorigin

--- src/main/java/com/myproject/ecommerce/dao/ProductRepository.java
+++ src/main/java/com/myproject/ecommerce/dao/ProductRepository.java
@@ -1,9 +1,16 @@
 package com.myproject.ecommerce.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.myproject.ecommerce.entity.Product;
 
+//accept calls from web browser scripts this origin
+
+//origin is more than the hostname
+//Origin -> protocol+hostname+port
+
+@CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
 }
